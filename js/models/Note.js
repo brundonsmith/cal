298dd8ee23a08aa00ac2938f470bc1485cc59afa
Brandon Smith <brandon.smith.945@gmail.com>
2017-05-28 15:02:37 -0500

    Added custom all notes route and change Note model a bit

--- js/models/Note.js
+++ js/models/Note.js
@@ -2,12 +2,18 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-const Note = mongoose.model('Note', new Schema({
+var NoteSchema = new Schema({
   user_id: { type: String, ref: 'User' },
   date: Date,
-  title: String,
-  body: String,
+  content: String,
   isMarkdown: Boolean,
-}));
+});
+NoteSchema.index({ content: 'text' });
+NoteSchema.methods.getTitle = function() {
+  return this.content.indexOf('\n') > -1 ? this.content.split('\n')[0] : this.content;
+};
+
+const Note = mongoose.model('Note', NoteSchema);
+
 
 module.exports = Note;
--- js/routes/note.js
+++ js/routes/note.js
@@ -1,12 +1,40 @@
 
 var mongoose = require('mongoose');
 var crud = require('./_basic_crud');
+var middleware = require('../middleware');
 
 const Note = mongoose.model('Note');
 
 module.exports = function(app) {
 
-    crud.defineReadAll(Note, app);
+    //crud.defineReadAll(Note, app);
+    app.get('/note/all', middleware.authenticate, middleware.decodeSession, function (req, res) {
+      var pageLength = req.query.pageLength;
+      var pageNumber = req.query.pageNumber;
+      var searchString = req.query.q;
+      // TODO: Implement paging
+
+      var criteria;
+      if(searchString) {
+        criteria = {
+          user_id: req.username,
+          $text : { $search : searchString },
+        }
+      } else {
+        criteria = {
+          user_id: req.username,
+        }
+      }
+      
+      Note.find(criteria)
+        .catch(function(err) {
+          console.error(err);
+          res.sendStatus(500);
+        })
+        .then(function(objects) {
+          res.json(objects);
+        });
+    });
 
     crud.defineRead(Note, app);
 
@@ -15,5 +43,4 @@ module.exports = function(app) {
     crud.defineUpdate(Note, app);
 
     crud.defineDelete(Note, app);
-
 };
